1b3843340527e4c1865292c8ffba8a848b09f1e7
DogMey <kevinsantiagoram25@gmail.com>
2025-06-14 20:25:08 -0500

    Cambiando solución para mejor entendimiento (IsPalindrome)

--- ValidPalindrome/Solution.py
+++ ValidPalindrome/Solution.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def isPalindrome(self, s):
-        s = ''.join(c.lower() for c in s if c.isalnum())    # Convertimos la cadena a minúsculas y filtramos solo caracteres alfanuméricos
-        return s == s[::-1]                                 # Comparamos la cadena con su reverso
+        caracteres_validos = []
+
+        for c in s:
+            if c.isalnum():                             # Verifica si es una letra o número
+                minuscula = c.lower()                   # Convierte a minúscula
+                caracteres_validos.append(minuscula)    # Lo agrega a la lista
+
+        s = ''.join(caracteres_validos) # Une todos los caracteres en una sola cadena
+        return s == s[::-1]             # Comparamos la cadena con su reverso
     
 solution = Solution()
 s = "A man, a plan, a canal: Panama"
